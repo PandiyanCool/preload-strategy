55ebaeffe6616de5043bca8eb12b4b0dfcf94250
Pandiyan <pandiyanit7@gmail.com>
2021-01-24 18:14:03 +0530

    Added the preload strategy code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadingStrategy, NoPreloading, PreloadAllModules } from '@angular/router';
+import { OptInStrategy } from './strategy';
+import { NetworkAwarePreloadStrategy } from './strategy/Network-Strategy';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
+    data: { preload: true }
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule),
+    data: { preload: false }
+  }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    // preloadingStrategy: NoPreloading,
+    // preloadingStrategy: PreloadAllModules,
+    preloadingStrategy: OptInStrategy,
+    // preloadingStrategy: NetworkAwarePreloadStrategy
+  })],
+  exports: [RouterModule],
+  providers: [OptInStrategy]
 })
 export class AppRoutingModule { }
---
+++ src/app/strategy/index.ts
@@ -0,0 +1,1 @@
+export * from './Opt-In-strategy';
